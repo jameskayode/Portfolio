107fe7f77ef1ae561c77974dbf10b6578fc6d82a
James Kayode <Jameskayode39@gmail.com>
2023-02-03 17:33:03 +0100

    third commit

---
+++ portfolio/app/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
+//import Nav from "./Components/Nav";
+//import Newnav from "./Components/newnav";
+//import NotFound from "./components/NotFound";
+import Projects from "./Components/Projects";
+import Tab from "./Components/Tab";
+//import Topnav from "./Components/Topnav";
+
+
+
+
+function App() {
+  return (
+    
+      <>
+      
+   <Tab/>
+
+     <Hero/>
+     <About/>
+     <Projects/>
+     <Footer/>
+     
+
+     </>
+   
+  );
+}
+
+export default App;
